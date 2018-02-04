265baa91bf2b8097640c36df595a7d24c53bff5e
Yaya Du <Yayadu@Yayas-MBP.fios-router.home>
2018-02-04 18:27:51 -0500

    furniture info feature added

---
+++ client/VR_Components/components/FurniButton.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {Animated, Image, Text, View, VrButton, NativeModules} from 'react-vr';
+
+export default class FurniButton extends React.Component{
+    constructor(props){
+        super();
+        this.state = {
+            //check whether hove over
+            gazed: 0
+        }
+    }
+
+    handleEnter(){
+        this.setState({gazed : 1})
+    }
+
+    handleExit(){
+        this.setState({gazed : 0})
+    }
+
+    handleClick(){
+        NativeModules.LinkingManager.openURL(this.props.uri)
+    }
+
+    render(){
+        const PPM = this.props.pixelsPerMeter;
+        const factor = this.props.factor;
+        const Pactor = PPM * factor
+
+        return(
+            <VrButton
+                style = {{
+                    flexDirection:'column',
+                    layoutOrigin:[0.5,0.5],
+                    alignItems:'flex-start',
+                    position: 'absolute',
+                    transform:[
+                        {translateX: this.props.translateX},
+                        {translateY: this.props.translateZ}
+                    ]
+            }}
+                onClick = {this.handleClick.bind(this)}>
+                <View
+                    style = {{
+                        flexDirection:'column',
+                        alignItems:'flex-start',
+                        backgroundColor:'rgba(0,0,0,0.4)',
+                        padding: 0.05 * PPM,
+                        borderColor:"white",
+                        borderWidth: 0.01 * PPM,
+                        opacity:this.state.gazed
+                    }}
+                    onExit = {this.handleExit.bind(this)}>
+                    <Text
+                    style = {{
+                        backgroundColor:'rgba(0,0,0,0)',
+                        color:'white',
+                        fontSize:20,
+                        fontWeight:"400",
+                        textAlign:'center',
+                        textAlignVertical:'auto',
+                        opacity: 2
+                    }}>
+                        {this.props.title}
+                    </Text>
+                    <Text
+                    style = {{
+                        backgroundColor:'rgba(0,0,0,0)',
+                        color:'white',
+                        fontSize:16,
+                        textAlign:'center',
+                        textAlignVertical:'auto',
+                        opacity:2
+                    }}>
+                        {this.props.text}
+                    </Text>
+                </View>
+                <Image
+                    style = {{
+                        height: 0.3* PPM,
+                        width: 0.3 * PPM,
+                        marginTop:0.15 * PPM,
+
+                    }}
+                    source = {{uri:"../static_assets/furniture.png"}}
+                    onEnter = {this.handleEnter.bind(this)}/>
+            </VrButton>
+        )}
+}
