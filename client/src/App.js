d5c0e2e247d893d4a2bdc87fe8a7b3da9ace841e
Huang <jhuang@webmdhealth.net>
2018-02-05 12:27:26 -0500

    took out footer

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,6 @@ const App = () => {
           <Route exact path = '/signup' component = {SignUpPage}/>
           <Route exact path = '/login' component = {LoginPage}/>
           <Route exact path="/logout"  render={() => (<Redirect to="/"/>)}/>
-          <Footer />
           {/* <Footer /> */}
         </div>
     </Router>
