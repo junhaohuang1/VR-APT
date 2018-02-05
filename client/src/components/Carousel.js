4e1657d354e868ca2aceaf4720a2b6c80ee3f07b
Daigunder35 <ajohnsen13@gmail.com>
2018-02-04 23:41:10 -0500

    carousel changes

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -9,7 +9,7 @@ export default () => {
 	images={[
 		'https://i.ytimg.com/vi/Rd3cG0-5qWY/maxresdefault.jpg',
 		'https://upload.wikimedia.org/wikipedia/commons/b/bf/St_Patrick%27s_Cathedral_Choir_360x180%2C_Dublin%2C_Ireland.jpg',
-		'http://www.freegreatpicture.com/files/36/7768-degree-seamless-panoramic.jpg',
+		'http://scion-social.com/blog/wp-content/uploads/2016/07/360-panorama-matador-seo.jpg',
 	]}
 >
 </Carousel>
