a5df937dcc789200756a4ce51cd7c5bc7f564e9b
Huang <jhuang@webmdhealth.net>
2018-02-07 19:13:43 -0500

    adding redux router

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { BrowserRouter as Router} from 'react-router-dom';
 import { Redirect, Route } from 'react-router'
 import LoginPage from './containers/LoginPage.js';
 import HomePage from './containers/HomePage.js';
 import SignUpPage from './containers/SignUpPage.js';
 import NavBar from './containers/NavBar.js';
 import Auth from './Auth';
-import CarouselPage from './components/Carousel'
-// import Footer from './components/Footer'
-
+import CarouselPage from './components/Carousel';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter } from 'react-router-redux';
+import { userActions } from './actions';
+import { connect } from 'react-redux';
 
 // remove tap delay, essential for MaterialUI to work properly
-// <Router history={browserHistory} routes={routes} />
-//
+const history = createHistory();
 injectTapEventPlugin();
 const App = () => {
   return (
     <MuiThemeProvider muiTheme={getMuiTheme()}>
-    <Router>
+    <ConnectedRouter history={history}>
         <div>
           <NavBar/>
           <Route exact path = '/' component = {Auth.isUserAuthenticated()?
@@ -30,8 +30,25 @@ const App = () => {
           <Route exact path = '/login' component = {LoginPage}/>
           <Route exact path="/logout"  render={() => (<Redirect to="/"/>)}/>
         </div>
-    </Router>
+    </ConnectedRouter>
     </MuiThemeProvider>
   )
 }
-export default App;
+function mapStateToProps(state) {
+  return {
+    loggedIn: state.authentication.loggedIn
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    login: (email,password) => {
+      dispatch(userActions.login(email,password))
+    },
+    updateSignInForm:(key, value) =>{
+      dispatch(userActions.updateSignInForm(key, value))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- client/src/store.js
+++ client/src/store.js
@@ -3,14 +3,18 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
 import promiseMiddleware from 'redux-promise-middleware';
-
+import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux';
+const history = createHistory();
 const loggerMiddleware = createLogger();
+
 
 export const store = createStore(
     rootReducer,
     applyMiddleware(
         thunkMiddleware,
         loggerMiddleware,
-        promiseMiddleware()
+        promiseMiddleware(),
+        routerMiddleware(history)
     )
 );
