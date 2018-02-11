7b054f9dd84c5a0dd05b304b3a04bb4cd1f3e09b
Yaya Du <Yayadu@Yayas-MBP.fios-router.home>
2018-02-11 15:26:30 -0500

    adjust styles

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -5,11 +5,12 @@ import axios from "axios";
 const styles = {
     button:{
         "position":"absolute",
-        "right": "50%",
-        "top": "80%"
+        "right": "45%",
+        "top": "50%"
     },
     img:{
-        "position": "relative"
+        "position": "relative",
+        "height": "auto"
     }
 }
 
@@ -41,19 +42,19 @@ export default class CarouselPage extends React.Component {
 
             <Carousel options = {{fullWidth: true}}>
                 <div className = "caroImg" style = {styles.img}>
-                    <img src = "https://i.ytimg.com/vi/Rd3cG0-5qWY/maxresdefault.jpg" />
+                    <img src = "/images/1.jpg" />
                     <button className='btn' id = "1" onClick ={this.handleClick} style = {styles.button}>Launch</button>
                 </div>
                 <div className = "caroImg" >
-                    <img src = "https://upload.wikimedia.org/wikipedia/commons/b/bf/St_Patrick%27s_Cathedral_Choir_360x180%2C_Dublin%2C_Ireland.jpg" style = {styles.img}/>
+                    <img src = "/images/2.jpg" style = {styles.img}/>
                     <button className='btn' id = "2" onClick ={this.handleClick} style = {styles.button}>Launch</button>
                 </div>
                 <div className = "caroImg" >
-                    <img src = "http://scion-social.com/blog/wp-content/uploads/2016/07/360-panorama-matador-seo.jpg" style = {styles.img}/>
+                    <img src = "/images/3.jpg" style = {styles.img}/>
                     <button className='btn' id = "3" onClick ={this.handleClick} style = {styles.button} >Launch</button>
                 </div>
                 <div className = "caroImg" >
-                    <img src = "https://upload.wikimedia.org/wikipedia/commons/b/bf/St_Patrick%27s_Cathedral_Choir_360x180%2C_Dublin%2C_Ireland.jpg"style = {styles.img}  />
+                    <img src = "/images/4.jpg"style = {styles.img}  />
                     <button className='btn' id = "4" onClick ={this.handleClick} style = {styles.button}>Launch</button>
                 </div>
             </Carousel>
