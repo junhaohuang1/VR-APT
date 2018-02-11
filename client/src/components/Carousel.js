f476ff800e7cd5c9ada12dbb349eb958e86289e1
Yaya Du <Yayadu@Yayas-MBP.fios-router.home>
2018-02-11 13:01:27 -0500

    button absolute

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -5,11 +5,11 @@ import axios from "axios";
 const styles = {
     button:{
         "position":"absolute",
-        "right": "40%",
-        "top": "60%"
+        "right": "50%",
+        "top": "80%"
     },
     img:{
-        "display": "relative"
+        "position": "relative"
     }
 }
 
@@ -27,15 +27,15 @@ export default class CarouselPage extends React.Component {
                     <img src = "https://i.ytimg.com/vi/Rd3cG0-5qWY/maxresdefault.jpg" />
                     <button className='btn' id = "1" onClick ={this.handleClick} style = {styles.button}>Launch</button>
                 </div>
-                <div className = "caroImg">
+                <div className = "caroImg" style = {styles.img}>
                     <img src = "https://upload.wikimedia.org/wikipedia/commons/b/bf/St_Patrick%27s_Cathedral_Choir_360x180%2C_Dublin%2C_Ireland.jpg" style = {styles.img}/>
                     <button className='btn' id = "2" onClick ={this.handleClick} style = {styles.button}>Launch</button>
                 </div>
-                <div className = "caroImg">
+                <div className = "caroImg" style = {styles.img}>
                     <img src = "http://scion-social.com/blog/wp-content/uploads/2016/07/360-panorama-matador-seo.jpg" style = {styles.img}/>
                     <button className='btn' id = "3" onClick ={this.handleClick} style = {styles.button} >Launch</button>
                 </div>
-                <div className = "caroImg">
+                <div className = "caroImg" style = {styles.img}>
                     <img src = "https://upload.wikimedia.org/wikipedia/commons/b/bf/St_Patrick%27s_Cathedral_Choir_360x180%2C_Dublin%2C_Ireland.jpg"style = {styles.img}  />
                     <button className='btn' id = "4" onClick ={this.handleClick} style = {styles.button}>Launch</button>
                 </div>
