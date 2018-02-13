6dd1ad326cf92f3f9cdf837987c0c0d659079428
Huang <jhuang@webmdhealth.net>
2018-02-12 19:33:20 -0500

    fixing routes syntax error

--- routes/scene.js
+++ routes/scene.js
@@ -10,29 +10,24 @@ router.route("/")
 
 // router.get("/", function(err,res){sceneController.findAll})
 // router.put("/", sceneController.update)
-// router.post("/image/:id"){
-//   var newNote = new Note(req.body);
-//   newNote.save(function(error, data) {
-//     if (error) throw error;
-//     Scene.findOneAndUpdate({
-//         "_id": req.params.id
-//       }, {
-//         $push: {
-//           "note": data._id
-//         }
-//       }, {
-//         new: true
-//       })
-//       .exec(function(error, data) {
-//         if (error) {
-//           res.sendStatus(400).json({
-//             message:"Please check your comment."
-//           })
-//         } else {
-//           res.sendStatus(200)
-//         }
-//       });
-//   });
-// }
+router.post("/scene/:id", function(req, res) {
+  var newNote = new Note(req.body);
+  newNote.save(function(error, data) {
+    if (error) throw error;
+    Scene.findOneAndUpdate({
+        "_id": req.params.id
+      }, {
+        $push: {
+          "note": data._id
+        }
+      }, {
+        new: true
+      })
+      .exec(function(error, data) {
+        if (error) throw error;
+        res.redirect("/saved");
+      });
+  });
+});
 
 module.exports = router;
